c3b7e6db1055796edfc3dfdd667a730c452160c3
Razial <uck15@bk.ru>
2023-07-02 13:16:49 +0300

    fixed bug and add wine_date.json

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/api/v1/<station>/<date>')
-def api(station, date):
-    #df = pd.read_csv("")
-    temperature = 23
-    return {'station': station,
-            'date': date,
-            'temperature': temperature}
+@app.route('/api/v1/<country>/<points>')
+def country(country, points):
+    df = pd.read_json('wine_date.json')
+    df = df.loc[df['points'] >= int(points)]
+    data = df.loc[df['country'] == country].to_dict(orient='records')
+    return data
+
+
+@app.route('/api/v1/<points>')
+def points(points):
+    df = pd.read_json('wine_date.json')
+    data = df.loc[df['points'] >= int(points)].to_dict(orient='records')
+    return data
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
